d1dfe2767fa1a5d08c898542fc356927efcc7bd4
mick-roper <mick.roper@symmetric.solutions>
2016-09-01 14:51:03 +0100

    Brought assembly info in line with release

--- Src/ObjectPool/Properties/AssemblyInfo.cs
+++ Src/ObjectPool/Properties/AssemblyInfo.cs
@@ -1,26 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ObjectPool")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ObjectPool")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("98667464-a12a-4528-9f98-67d479cfbaa1")]
+[assembly: AssemblyDescription("Thread safe object pool")]
+[assembly: AssemblyCompany("Mick-Roper")]
+[assembly: AssemblyProduct("Object Pool")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +18,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
